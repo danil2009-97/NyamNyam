f505dd07bfa7c214a3af3bd4dd832ea71eaead13
Ilya Korol <ikorol98@gmail.com>
2019-12-16 20:54:45 +0300

    With new pictures and buttons. Minor fixes.

--- app/src/main/java/HSE/MobileDev/NyamNyam/Views/MainActivity.java
+++ app/src/main/java/HSE/MobileDev/NyamNyam/Views/MainActivity.java
@@ -30,19 +30,8 @@ public class MainActivity extends AppCompatActivity {
                     true),
                     new Product(1,"Banana",R.drawable.banana,
                     true),
-                    new Product(2,"Smthng",R.drawable.sausage,
+                    new Product(2,"Sausage",R.drawable.sausage,
                     true)
+
                 ));
-
-
-
-
-//            new Product(1,1,"https://c7.hotpng.com/preview/575/426/540/cooking-banana-clip-art-banana.jpg",
-//                    true),
-//            new Product(2,2,"https://c7.hotpng.com/preview/322/96/938/thuringian-sausage-bratwurst-liverwurst-pelmeni-sausage-thumbnail.jpg",
-//                    true),
-//            new Product(3,3,"https://img2.freepng.ru/20181123/rx/kisspng-graham-bread-sourdough-bakery-small-bread-5bf8277eaad716.3975945715429896946998.jpg",
-//                    true);
-
-    //Test - GitCommit
 }
