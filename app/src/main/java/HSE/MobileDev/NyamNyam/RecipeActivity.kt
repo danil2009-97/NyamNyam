28cbc982c5ebe81462390cd8b09c41466a569c09
Daniil Levin <levin.dan@ya.ru>
2020-03-23 12:38:32 +0300

    Add recipe listing

--- app/src/main/java/HSE/MobileDev/NyamNyam/RecipeActivity.kt
+++ app/src/main/java/HSE/MobileDev/NyamNyam/RecipeActivity.kt
@@ -7,32 +7,104 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 import HSE.MobileDev.NyamNyam.Database.Model.Recipe
+import android.content.res.Configuration
+import android.os.PersistableBundle
 import android.view.View
+import java.util.*
+import kotlin.collections.ArrayList
 
 class RecipeActivity : AppCompatActivity() {
 
-    private val testRecipe = Recipe(0, "Сэндвич", "тест", R.drawable.shutterstock_525130276, null, 4,
-            "Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar.",
-            false)
+    private var currentIndex:Int = -1;
+    private var listSize:Int = -1;
+    private var recipeList = ArrayList<Recipe>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val lastInd = savedInstanceState?.getInt("Index")
+
+        if (lastInd != null) {
+            currentIndex = lastInd
+        } else {
+            currentIndex = -1
+        }
+
+        getRecipeList()
         setContentView(R.layout.constraintlayout_recipe)
-        fillWindow()
+        val initialRecipe:Recipe = getInitialRecipe()
+        fillWindow(initialRecipe)
     }
 
-    protected fun fillWindow() {
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("Index", currentIndex)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currentIndex = savedInstanceState.getInt("Index")
+    }
+
+    private fun getInitialRecipe () : Recipe{
+        val emptyRecipe = Recipe(-1, "Нет доступных рецептов!", "Упс...",
+                R.drawable.sad, null, 0,"Добавьте продукты в Ваш холодильник", false)
+
+        if (listSize > 0){
+            if (currentIndex>-1){
+                return recipeList[currentIndex]
+            }
+            else {
+                currentIndex = 0
+                return recipeList[currentIndex]
+            }
+        }
+        else{
+            return emptyRecipe
+        }
+    }
+
+    private fun getRecipeList(){
+        // request from db
+        recipeList.addAll(Arrays.asList(
+                Recipe(0, "Сэндвич", "тест", R.drawable.shutterstock_525130276, null, 4,
+                        "Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar. Long sample text to test scrollbar.",
+                        false),
+                Recipe(1, "Второй сэндвич с длинным названием", "тест", R.drawable.sausage, null, 3,
+                        "quite short text", false)
+        ))
+
+        listSize = recipeList.size
+
+    }
+
+
+    protected fun fillWindow(recipe:Recipe) {
         val productTitleTextView = findViewById<View>(R.id.productTitleTextView) as TextView
         val difficultyTitleTextView = findViewById<View>(R.id.difficultyTextView) as TextView
         val mealImageView = findViewById<View>(R.id.mealImageView) as ImageView
         val recipeTextView = findViewById<View>(R.id.recipeTextView) as TextView
 
-        val difficulty = String.format("Сложность: %1\$d/5", testRecipe.difficulty)
+        val difficulty = String.format("Сложность: %1\$d/5", recipe.difficulty)
 
-        mealImageView.setImageResource(testRecipe.imageSource)
-        productTitleTextView.text = testRecipe.name
+        mealImageView.setImageResource(recipe.imageSource)
+        productTitleTextView.text = recipe.name
         difficultyTitleTextView.setText(difficulty)
-        recipeTextView.text = testRecipe.guide
+        recipeTextView.text = recipe.guide
+    }
+
+    fun onNextClick(view: View){
+        if(currentIndex!=-1 && currentIndex<listSize-1){
+            currentIndex++
+            fillWindow(recipeList[currentIndex])
+        }
+    }
+
+    fun onPreviousClick(view: View){
+        if(currentIndex>0){
+            currentIndex--
+            fillWindow(recipeList[currentIndex])
+        }
     }
 
     fun closeActivity(view: View) {
