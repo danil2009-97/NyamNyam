c485bd2224fac3cd2a6e98cca32cbd51b9d2cad0
Daniil Levin <levin.dan@ya.ru>
2020-03-24 03:30:04 +0300

    Add product removing

--- app/src/main/java/HSE/MobileDev/NyamNyam/MainActivity.kt
+++ app/src/main/java/HSE/MobileDev/NyamNyam/MainActivity.kt
@@ -3,6 +3,7 @@ package HSE.MobileDev.NyamNyam
 import HSE.MobileDev.NyamNyam.Database.DatabaseHelper
 import HSE.MobileDev.NyamNyam.Database.Model.Product
 import HSE.MobileDev.NyamNyam.Database.Model.Recipe
+import HSE.MobileDev.NyamNyam.Views.ProductSimpleAdapter
 import HSE.MobileDev.NyamNyam.Views.ProductsAdapter
 import android.content.Intent
 import android.os.Bundle
@@ -10,9 +11,16 @@ import android.view.View
 import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat.startActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var productsAdapter : ProductsAdapter? = null
+    private var gridView : GridView? = null
 
     private val productList: MutableList<Product> = ArrayList()
     private var db: DatabaseHelper? = null
@@ -52,9 +60,33 @@ class MainActivity : AppCompatActivity() {
         val product_1: Long = db!!.createProduct(product, longArrayOf(1))
 //        db!!.createProductRecipe(1,1);
 
-        val gridView = findViewById<View>(R.id.gridview) as GridView
+        gridView = findViewById<View>(R.id.gridview) as GridView
         val productsAdapter = ProductsAdapter(this, products)
-        gridView.adapter = productsAdapter
+        gridView!!.adapter = productsAdapter
+
+        gridView!!.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            // Get the GridView selected/clicked item text
+            showActionsDialog(position)
+        };
+    }
+
+    private fun showActionsDialog(position: Int) {
+        val colors = arrayOf<CharSequence>("Да", "Нет")
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Удалить продукт?")
+        builder.setItems(colors) { dialog, which ->
+            if (which == 0) {
+                deleteProductFromFridge(position)
+                productsAdapter = ProductsAdapter(this, products)
+                gridView!!.adapter = productsAdapter
+            }
+        }
+        builder.show()
+    }
+
+    private fun deleteProductFromFridge(position: Int){
+        products.removeAt(position)
     }
 
     fun openRecipe(view: View) {
